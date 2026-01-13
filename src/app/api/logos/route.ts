99f9f9ff709fdfa30fed3214bc7dce4719074774
Chhel orngkeasela <chhelangkeasela@gmail.com>
2026-01-13 16:24:27 +0700

    Update Code

--- src/app/api/logos/route.ts
+++ src/app/api/logos/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from 'next/server';
 
 export async function GET() {
   try {
-    const res = await fetch('https://api-gpsf.vichea.dev/api/v1/logo'); // server-to-server
+    const res = await fetch('https://api-gpsf.datacolabx.com/api/v1/logo'); // server-to-server
     const data = await res.json();
     return NextResponse.json(data);
   } catch (err) {
